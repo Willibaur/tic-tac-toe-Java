1dfe99edb6fce6561835d5978436a781e834b3ba
William Bautista <williambautista@gmail.com>
2016-04-25 21:38:02 +0100

    Exception for X & Y axises and Input box added

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,8 +1,27 @@
 
 public class TicTacToe {
 
+    private Character[][] board = {{'O', 'O', 'O'},
+                                   {'O', 'O', 'O'},
+                                   {'O', 'O', 'O'}};
+
     public void play(int x, int y) {
-        if (x<1 || x>3) { throw new RuntimeException("X is outside board"); }
-        else if (y<1 || y>3) { throw new  RuntimeException("X is outside board"); }
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x,y);
+    }
+
+    private void setBox(int x, int y) {
+        if (board[x-1][y-1] != 'O') {
+            throw new RuntimeException("Box is occupied");
+        } else {
+            board[x-1][y-1] = 'X';
+        }
+    }
+
+    private void checkAxis(int axis) {
+        if (axis < 1 || axis > 3) {
+            throw new RuntimeException("X is outside board");
+        }
     }
 }
