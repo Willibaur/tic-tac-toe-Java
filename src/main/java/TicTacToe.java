b398ce55e33c55dcfb8ddb240c99f713f2715654
William Bautista <williambautista@gmail.com>
2016-04-25 20:34:10 +0100

    Exception for Y axis boundary board added

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -2,6 +2,7 @@
 public class TicTacToe {
 
     public void play(int x, int y) {
-        if (x<1 || x>3) throw new RuntimeException("X is outside board");
+        if (x<1 || x>3) { throw new RuntimeException("X is outside board"); }
+        else if (y<1 || y>3) { throw new  RuntimeException("X is outside board"); }
     }
 }
