51626203fada02dde76bc4c7409d9522d239591d
William Bautista <williambautista@gmail.com>
2016-04-26 18:50:50 +0100

    Win function refactored

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -12,9 +12,11 @@ public class TicTacToe {
     public String play(int x, int y) {
         checkAxis(x);
         checkAxis(y);
+
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        if (isWin()) {
+
+        if (isWin(x, y)) {
             return lastPlayer + " is the winner";
         } else if (isDraw()) {
             return "The result is draw";
@@ -33,22 +35,21 @@ public class TicTacToe {
         return  true;
     }
 
-    private boolean isWin() {
+    private boolean isWin(int x, int y) {
         int playerTotal = lastPlayer * SIZE;
-        char diagonal1 = '\0';
-        char diagonal2 = '\0';
+
+        char horizontal, vertical, diagonal1, diagonal2;
+
+        horizontal = vertical = diagonal1 = diagonal2 = '\0';
 
         for (int i = 0; i < SIZE; i++) {
-            diagonal1 += board[i][i];
-            diagonal2 += board[i][SIZE - i - 1];
-
-            if ((board[0][i] + board[1][i] + board[2][i]) == playerTotal) {
-                return true;
-            } else if ((board[i][0] + board[i][1] + board[i][2]) == playerTotal) {
-                return true;
-            }
+            horizontal += board[i][y-1];
+            vertical   += board[x-1][i];
+            diagonal1  += board[i][i];
+            diagonal2  += board[i][SIZE - i - 1];
         }
-        if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+        if (horizontal == playerTotal || vertical == playerTotal ||
+            diagonal1  == playerTotal || diagonal2 == playerTotal  ) {
             return true;
         }
         return false;
